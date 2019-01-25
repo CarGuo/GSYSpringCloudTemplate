44e1f91b9089565630a608220ed4c7e9a179c058
guoshuyu <guoshuyu@ygsoft.com>
2019-01-25 16:41:02 +0800

    init

--- gsy-auth/src/main/java/com/shuyu/springcloud/auth/config/SecurityConfiguration.java
+++ gsy-auth/src/main/java/com/shuyu/springcloud/auth/config/SecurityConfiguration.java
@@ -4,42 +4,48 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    @Override
-    protected UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("user_1").password("123456").authorities("USER").build());
-        manager.createUser(User.withUsername("user_2").password("123456").authorities("USER").build());
-        return manager;
-    }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        // @formatter:off
-        http
-                .requestMatchers().anyRequest()
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user_1").password("123456").authorities("USER")
                 .and()
-                .authorizeRequests()
-                .antMatchers("/oauth/*").permitAll();
-        // @formatter:on
+                .withUser("user_2").password("123456").authorities("USER");
     }
-
 
     @Bean
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         AuthenticationManager manager = super.authenticationManagerBean();
         return manager;
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new MyPasswordEncoder();
+    }
+
+    public static class MyPasswordEncoder implements PasswordEncoder{
+
+        @Override
+        public String encode(CharSequence charSequence) {
+            return charSequence.toString();
+        }
+
+        @Override
+        public boolean matches(CharSequence charSequence, String s) {
+            return s.equals(charSequence.toString());
+        }
+
     }
 }
